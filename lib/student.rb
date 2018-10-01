3c7dc1ecc39cf722b19c2633b32aadcb04638daf
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-01 17:46:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,11 +13,19 @@ class Student
   
   def self.create_table 
     sql = <<-SQL 
-    CREATE TABLE IF NOT EXISTS students (
-    id INTEGER PRIMARY KEY,
-    name TEXT, 
-    grade TEXT
-    )
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT, 
+        grade TEXT
+        )
+        SQL 
+      DB[:conn].execute(sql)
+  end 
+  
+  def self.drop_table 
+    sql = "DROP TABLE students" 
+  
+    
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
